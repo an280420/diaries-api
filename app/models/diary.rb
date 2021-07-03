95b352e21cc5c5c325a53238a716e816fa1c9d0c
@an280420 <angit280420@gmail.com>
2021-07-03 22:14:49 +0500

    Add scope expired

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -5,4 +5,5 @@ class Diary < ApplicationRecord
   validates :expiration, absence: true, if: :public_kind?
 
   has_many :notes, dependent: :destroy
+  scope :expired, -> { where('expiration < ?', Time.now) }
 end
