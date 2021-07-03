2ebe90b36cd1d212d4c19fe13cf164288ab5a1a7
@an280420 <angit280420@gmail.com>
2021-07-03 11:40:09 +0500

    Edit CRUD

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -1,6 +1,8 @@
 module API
   module V1
     class DiariesController < BaseController
+      before_action :set_diary, except: [:index, :new, :create]
+      
       def create
         diary = Diary.new(diary_params)
 
@@ -15,7 +17,28 @@ module API
         render_success Diary.all
       end
 
+      def show
+        render_success [@diary]
+      end
+
+      def update
+        if @diary.update(diary_params)
+          render_success [@diary]
+        else
+          render_errors errors: @diary.errors.full_messages, status: 422
+        end
+      end
+
+      def destroy
+        @diary.destroy
+        render json: @diary, status: 204 
+      end
+
       private
+
+      def set_diary
+        @diary = Diary.find(params[:id])
+      end
 
       def diary_params
         params.require(:diary).permit(:title, :enum, :expiration)
--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,8 +1,11 @@
 module API
   module V1
     class NotesController < BaseController
+      before_action :set_note, except: [:index, :new, :create]
+
       def create
-        note = Note.new(note_params)
+        note = find_diary(params[:diary_id]).notes.new(note_params)
+        # note = Note.new(note_params)
 
         if note.save
           render_success [note]
@@ -12,10 +15,36 @@ module API
       end
 
       def index
-        render_success Note.sorted
+        notes = find_diary(params[:diary_id]).notes
+        render_success [notes]
+      end
+
+      def show
+        render_success [@note]
+      end
+
+      def update
+        if @note.update(note_params)
+          render_success [@note]
+        else
+          render_errors errors: @note.errors.full_messages, status: 422
+        end
+      end
+
+      def destroy
+        @note.destroy
+        render json: @note, status: 204 
       end
 
       private
+
+      def find_diary(diary_id)
+        Diary.find(diary_id)
+      end
+
+      def set_note
+        @note = Note.find(params[:id])
+      end
 
       def note_params
         params.require(:note).permit(:text)
