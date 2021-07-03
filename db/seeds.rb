0b63e098e764effb388b0439dc791980307cc6f9
@an280420 <angit280420@gmail.com>
2021-07-03 10:49:24 +0500

    create example data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Diary.destroy_all
+
+time_expiration = DateTime.now + 600
+
+diaries = [
+  diary1 = Diary.create(title: "Дневник1", kind: "public"),
+  diary2 = Diary.create(title: "Дневник2", kind: "private"),
+  diary3 = Diary.create(title: "Дневник3", kind: "private", expiration: time_expiration),
+  diary4 = Diary.create(title: "Дневник4", kind: "private", expiration: time_expiration)
+]
+
+diaries.each do |diary|
+  Note.create(diary_id: diary.id, text: 'Первая запись')
+  Note.create(diary_id: diary.id, text: 'Вторая запись')
+  Note.create(diary_id: diary.id, text: 'Третья запись')
+end
