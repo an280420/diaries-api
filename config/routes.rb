20f3cea6e93237be778fefd9a2d1e90862d73803
@an280420 <angit280420@gmail.com>
2021-07-03 23:45:53 +0500

    Add sidekiq/web

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => "/sidekiq"
 end
